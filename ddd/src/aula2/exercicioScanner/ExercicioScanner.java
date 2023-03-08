69268980b74fc2573e4855b4f43d835dc2f618df
Pedro Carvalho Pacheco <cpedropacheco@hotmail.com>
2023-03-08 09:19:11 -0300

    changing ints to doubles

--- ddd/src/aula2/exercicioScanner/ExercicioScanner.java
+++ ddd/src/aula2/exercicioScanner/ExercicioScanner.java
@@ -7,13 +7,13 @@ public class ExercicioScanner {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Digite a sua primeiro: ");
-		int nota1 = input.nextInt();
+		double nota1 = input.nextInt();
 		
 		System.out.println("Digite a sua segunda nota:");
-		int nota2 = input.nextInt();
+		double nota2 = input.nextInt();
 		
 		System.out.println("Digite a sua terceira nota:");
-		int nota3 = input.nextInt();
+		double nota3 = input.nextInt();
 		input.close();
 		
 		double media = (nota1 + nota2 + nota3) / 3;
