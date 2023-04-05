4849d7a85144ab1e4d3ef9a057820ac6c862b399
pedrocpacheco <pedrocpacheco@hotmail.com>
2023-04-05 08:43:54 -0300

    fazendo switch case de notas

---
+++ ddd/src/aula7/exemploStrings.java
@@ -0,0 +1,14 @@
+package aula7;
+
+import java.util.Scanner;
+
+public class exemploStrings {
+
+	public static void main(String[] args) {
+		
+	Scanner cc = new Scanner(System.in);
+		
+		
+	}
+	
+}
--- ddd/src/aula7/lacos.java
+++ ddd/src/aula7/lacos.java
@@ -1,9 +1,29 @@
 package aula7;
 
+import java.util.Scanner;
+
 public class lacos {
 
 	public static void main(String[] args) {
 		System.out.println("Teste aula 7");
+		Scanner cc = new Scanner(System.in);
+		
+		System.out.println("Digite um Numero");
+		int numero = cc.nextInt();
+		
+		switch(numero){
+		
+		case 1:
+		case 2:
+			System.out.println("Digitou 1 ou 2");
+			break;
+		default:
+			System.out.println();
+		}
+		
+		
+		
+		
 	}
 	
 }
